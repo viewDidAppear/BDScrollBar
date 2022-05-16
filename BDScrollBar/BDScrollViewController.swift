a1065b561d195fe6ab0ae2e2189565871c140c69
Benjamin Deckys <ben.kawabata@gmail.com>
2022-05-16 22:32:31 +0900

    [BDSCR-4] Restructure Project

--- BDScrollBar/BDScrollViewController.swift
+++ BDScrollBar/BDScrollViewController.swift
@@ -9,22 +9,23 @@ import UIKit
 
 class BDScrollViewController: UITableViewController {
 
+	private lazy var scrollBar: BDScrollBar = {
+		BDScrollBar() // BDScrollBar(style: .modern)
+	}()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		let scrollBar = BDScrollBar(style: .modern)
-		self.tableView.bd_addScrollBar(scrollBar)
-		self.tableView.separatorInset = scrollBar.adjustedSeparatorInsets(for: self.tableView.separatorInset)
-		self.tableView.backgroundColor = .white
+		tableView.bd_addScrollBar(scrollBar)
+		tableView.separatorInset = scrollBar.adjustedSeparatorInsets(for: tableView.separatorInset)
 	}
 
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		100
-	}
+}
 
+extension BDScrollViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		var listConfig = UIListContentConfiguration.cell()
-		listConfig.textProperties.color = .black
+		listConfig.textProperties.color = UIColor(named: "Text")!
 		listConfig.text = String(format: "Cell %ld", indexPath.row)
 
 		let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
@@ -34,14 +35,22 @@ class BDScrollViewController: UITableViewController {
 
 		return cell
 	}
+}
 
+extension BDScrollViewController {
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		100
+	}
+}
+
+extension BDScrollViewController {
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
 	}
+}
 
+extension BDScrollViewController {
 	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		44
 	}
-
 }
-
